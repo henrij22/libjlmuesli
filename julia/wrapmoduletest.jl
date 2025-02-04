e99674e5bd0978dd97aebc95bc1127abf9958ae4
Henrik Jakob <jakob@ibb.uni-stuttgart.de>
2025-02-04 15:30:29 +0100

    fix3

---
+++ julia/wrapmoduletest.jl
@@ -0,0 +1,20 @@
+# SPDX-FileCopyrightText: 2025 Henrik Jakob jakob@ibb.uni-stuttgart.de
+# SPDX-License-Identifier: MIT
+
+using LinearAlgebra
+
+module MuesliTest
+path_to_lib = "/workspaces/libjlmuesli/build/lib/"
+
+if (!isempty(ARGS))
+    path_to_lib = ARGS[1]
+end
+
+using CxxWrap
+@wrapmodule(()->joinpath(path_to_lib, "libjlmuesli"))
+
+function __init__()
+    @initcxx
+end
+
+end
