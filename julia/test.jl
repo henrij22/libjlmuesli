ea0ac34d4364c4a384b7c2fe3cd85da434c82b5a
Henrik Jakob <jakob@ibb.uni-stuttgart.de>
2025-03-12 18:26:52 +0100

    uncomment test

--- julia/test.jl
+++ julia/test.jl
@@ -40,59 +40,8 @@ MuesliTest.setProperty!(properties, "young", Emod)
 MuesliTest.setProperty!(properties, "poisson", ν)
 MuesliTest.getProperty(properties, "young")
 
-vec = MuesliTest.Ivector(1.0, 2.0, 3.0)
-vec = [1.0, 2.0, 3.0]
-MuesliTest.Itensor(vec, vec, vec)
-T = [1.0 2.0 3.0
-     4.0 5.0 6.0
-     5.0 3.0 2.3]
-MuesliTest.Itensor(T)
-
-Base.IndexStyle(::MuesliTest.Itensor) = IndexLinear()
-Base.getindex(v::MuesliTest.Itensor, i::Int, j::Int) = MuesliTest.cxxgetindex(v, i, j)[]
-function Base.setindex!(v::MuesliTest.Itensor, val, i::Int, j::Int)
-    MuesliTest.cxxsetindex!(v, convert(Float64, val), i, j)
-end
-
-Base.IndexStyle(::MuesliTest.Itensor4) = IndexLinear()
-function Base.getindex(v::MuesliTest.Itensor4, i::Int, j::Int, k::Int, l::Int)
-    MuesliTest.cxxgetindex(v, i, j, k, l)[]
-end
-function Base.setindex!(v::MuesliTest.Itensor4, val, i::Int, j::Int, k::Int, l::Int)
-    MuesliTest.cxxsetindex!(v, convert(Float64, val), i, j, k, l)
-end
-
-t = MuesliTest.Itensor(T)
-t2 = MuesliTest.Istensor(T)
-
-C = rand(3, 3, 3, 3);
-ten4 = MuesliTest.Itensor4(C)
-
-nothing
-# function Base.show(io::IO, obj::MuesliTest.Itensor)
-#     printObject(io, obj, multiline = false)
-# end
-
-# function Base.show(io::IO, mime::MIME"text/plain", obj::MuesliTest.Itensor)
-#     multiline = get(io, :multiline, true)
-#     printObject(io, obj, multiline = multiline)
-# end
-
-# function printObject(io::IO, obj::MuesliTest.Itensor; multiline::Bool)
-#     if multiline
-#         # print("ITensor")
-#         # print("\n")
-#         for i in 1:3
-#             print("$(obj(i, 1)) $(obj(i, 2)) $(obj(i, 3))\n")
-#         end
-#     else
-#         # print("ITensor")
-#     end
-# end
-
 # t = MuesliTest.Itensor(T)
 
-nothing
 # MuesliTest.getProperty(mat, MuesliTest.PR_POISSON)
 
 # mat2 = MuesliTest.ElasticIsotropicMaterial(properties)
@@ -102,12 +51,13 @@ nothing
 
 # mp = MuesliTest.ElasticIsotropicMP(mat)
 
-C = [0.600872 -0.179083 0
-     -0.179083 0.859121 0
-     0 0 1]
+# C = [0.600872 -0.179083 0
+#      -0.179083 0.859121 0
+#      0 0 1]
+# C = MuesliTest.Itensor(C)
 
-F_ = sqrt(C)
-F = MuesliTest.Itensor(F_)
+# F_ = sqrt(C)
+# F = MuesliTest.Itensor(F_)
 
 # tensors = MuesliTest.ArrayOfIsTensors()
 # MuesliTest.push!(tensors, C)
@@ -117,7 +67,7 @@ F = MuesliTest.Itensor(F_)
 # using Tensors
 # MuesliTest.updateCurrentState(mp, 1.0, C)
 
-σ = MuesliTest.Istensor()
+# σ = MuesliTest.Istensor()
 # ℂ = zeros(3, 3, 3, 3);
 
 # MuesliTest.stress!(mp, σ)
@@ -127,10 +77,10 @@ F = MuesliTest.Itensor(F_)
 # MuesliTest.tangentTensor!(mp, ℂ)
 # tovoigt(Array, SymmetricTensor{4, 3}(ℂ))
 
-nh = MuesliTest.NeoHookeMaterial(properties)
-nhmp = MuesliTest.NeoHookeMP(nh)
-MuesliTest.updateCurrentState(nhmp, 0.0, F)
-MuesliTest.secondPiolaKirchhoffStress!(nhmp, σ)
+# nh = MuesliTest.NeoHookeMaterial(properties)
+# nhmp = MuesliTest.NeoHookeMP(nh)
+# MuesliTest.updateCurrentState(nhmp, 0.0, F)
+# MuesliTest.secondPiolaKirchhoffStress!(nhmp, σ)
 
 # σ
 # MuesliTest.convectedTangent!(nhmp, ℂ)
